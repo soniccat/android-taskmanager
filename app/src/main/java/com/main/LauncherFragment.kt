d0dde2084ef72d0e56930968d1e2240196ef2d92
Alexey Glushkov <soniccat@list.ru>
2020-01-05 22:07:04 +0300

    converted launcher to fragment

---
+++ app/src/main/java/com/main/LauncherFragment.kt
@@ -0,0 +1,157 @@
+package com.main
+
+import android.content.Context
+import android.content.Intent
+import android.os.Bundle
+import android.util.Log
+import android.view.*
+import android.widget.AdapterView.OnItemClickListener
+import android.widget.ArrayAdapter
+import androidx.fragment.app.Fragment
+import com.example.alexeyglushkov.authorization.requestbuilder.HttpUrlConnectionBuilder
+import com.example.alexeyglushkov.authtaskmanager.HttpServiceCommand
+import com.example.alexeyglushkov.authtaskmanager.ServiceTaskProvider
+import com.example.alexeyglushkov.authtaskmanager.ServiceTaskRunner
+import com.example.alexeyglushkov.cachemanager.Storage
+import com.example.alexeyglushkov.cachemanager.clients.Cache
+import com.example.alexeyglushkov.cachemanager.clients.SimpleCache
+import com.example.alexeyglushkov.cachemanager.disk.DiskStorage
+import com.example.alexeyglushkov.service.SimpleService
+import com.example.alexeyglushkov.streamlib.convertors.BytesStringConverter
+import com.example.alexeyglushkov.taskmanager.task.Task
+import com.example.alexeyglushkov.taskmanager.task.TaskImpl
+import com.main.Networks.Network
+import com.rssclient.controllers.R
+import com.rssclient.controllers.databinding.ActivityMainBinding
+import com.rssclient.controllers.databinding.FragmentLauncherBinding
+import com.rssclient.rssfeed.view.MainRssActivity
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.MainScope
+import kotlinx.coroutines.launch
+import kotlinx.coroutines.withContext
+
+
+class LauncherFragment : Fragment() {
+    private val scope = MainScope()
+    private var binding: FragmentLauncherBinding? = null
+
+    private var service: SimpleService? = null
+    private var storage: Storage? = null
+
+    private val mainApplication = MainApplication.instance
+    val taskManager = mainApplication.taskManager
+    val accountStore = mainApplication.accountStore
+
+    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
+        binding = FragmentLauncherBinding.inflate(inflater, container, false)
+        return binding!!.root
+    }
+
+    override fun onDestroyView() {
+        super.onDestroyView()
+        binding = null
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        val listView = binding!!.list
+        listView.adapter = ArrayAdapter(view.context, android.R.layout.activity_list_item, android.R.id.text1, arrayOf("Rss Client", "Authorization", "Run Request", "Clear cache", "Load Sets"))
+        listView.onItemClickListener = OnItemClickListener { _, _, position, _ ->
+            if (position == 0) {
+                showRssClient(view.context)
+            } else if (position == 1) {
+                showAuthorization()
+            } else if (position == 2) {
+                scope.launch { requestUser() }
+            } else if (position == 3) {
+                clearCache()
+            } else if (position == 4) {
+                scope.launch { loadSets(true) }
+            }
+        }
+    }
+
+    private fun showRssClient(context: Context) {
+        val intent = Intent(context, MainRssActivity::class.java)
+        startActivity(intent)
+    }
+
+    private fun showAuthorization() {
+        val authTask: Task = object : TaskImpl() {
+            override suspend fun startTask() {
+                val account = Networks.createAccount(Network.Quizlet)
+                val creds = withContext(Dispatchers.IO) {
+                    account.authorize()
+                }
+
+                Log.d(TAG, "showAuthorization onFinished " + creds.isValid)
+            }
+        }
+        taskManager.addTask(authTask)
+    }
+
+    private suspend fun requestUser() {
+        if (service == null) {
+            initService()
+        }
+        val builder = HttpUrlConnectionBuilder()
+        builder.setUrl("https://api.foursquare.com/v2/users/self?v=20140806&m=foursquare")
+        val cmd: HttpServiceCommand<*> = HttpServiceCommand<String>(builder, BytesStringConverter())
+        val cache = SimpleCache(storage!!)
+        cache.cacheMode = Cache.CacheMode.CHECK_CACHE_IF_ERROR_THEN_LOAD
+        cmd.setCacheClient(cache)
+        service!!.runCommand(cmd, true)
+    }
+
+    private fun initService() {
+        service = SimpleService()
+
+        val accounts = accountStore.getAccounts(Network.Quizlet.ordinal)
+        val serviceAccount = if (accounts.size > 0) {
+            accounts[0]
+        } else {
+            Networks.createAccount(Network.Quizlet)
+        }
+
+        service!!.account = serviceAccount
+        // TODO: we need the solution for id
+        service!!.setServiceCommandProvider(ServiceTaskProvider())
+        service!!.setServiceCommandRunner(ServiceTaskRunner(taskManager, "31234"))
+        storage = serviceCache
+    }
+
+    private val serviceCache: Storage
+        get() {
+            val cacheDir = context!!.getDir("ServiceCache", Context.MODE_PRIVATE)
+            return DiskStorage(cacheDir)
+        }
+
+    private fun clearCache() {
+        try {
+            accountStore.removeAll()
+            serviceCache.removeAll()
+        } catch (e: Exception) {
+            e.printStackTrace()
+        }
+    }
+
+    private suspend fun loadSets(useCache: Boolean) {
+        mainApplication.quizletService.loadSets(null)
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu, inflater: MenuInflater) {
+        inflater.inflate(R.menu.menu_main, menu)
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        val id = item.itemId
+        return if (id == R.id.action_settings) {
+            true
+        } else super.onOptionsItemSelected(item)
+    }
+
+    companion object {
+        private const val TAG = "LauncherFragment"
+    }
+}
--- app/src/main/java/com/main/MainActivity.kt
+++ app/src/main/java/com/main/MainActivity.kt
@@ -1,152 +1,33 @@
 package com.main
 
-import android.content.Context
-import android.content.Intent
 import android.os.Bundle
-import android.util.Log
-import android.view.Menu
-import android.view.MenuItem
-import android.widget.AdapterView.OnItemClickListener
-import android.widget.ArrayAdapter
-import com.example.alexeyglushkov.authorization.requestbuilder.HttpUrlConnectionBuilder
-import com.example.alexeyglushkov.authtaskmanager.HttpServiceCommand
-import com.example.alexeyglushkov.authtaskmanager.ServiceTaskProvider
-import com.example.alexeyglushkov.authtaskmanager.ServiceTaskRunner
-import com.example.alexeyglushkov.cachemanager.Storage
-import com.example.alexeyglushkov.cachemanager.clients.Cache
-import com.example.alexeyglushkov.cachemanager.clients.SimpleCache
-import com.example.alexeyglushkov.cachemanager.disk.DiskStorage
-import com.example.alexeyglushkov.service.SimpleService
-import com.example.alexeyglushkov.streamlib.convertors.BytesStringConverter
-import com.example.alexeyglushkov.taskmanager.task.Task
-import com.example.alexeyglushkov.taskmanager.task.TaskImpl
-import com.main.Networks.Network
-import com.rssclient.controllers.R
+import androidx.fragment.app.Fragment
+import androidx.fragment.app.FragmentFactory
 import com.rssclient.controllers.databinding.ActivityMainBinding
-import com.rssclient.rssfeed.view.MainRssActivity
-import kotlinx.coroutines.Dispatchers
-import kotlinx.coroutines.MainScope
-import kotlinx.coroutines.launch
-import kotlinx.coroutines.withContext
 
-
-class MainActivity : BaseActivity() {
-    private val scope = MainScope()
-    private lateinit var binding: ActivityMainBinding
-
-    private var service: SimpleService? = null
-    private var storage: Storage? = null
-
-    private val mainApplication = MainApplication.instance
-    val taskManager = mainApplication.taskManager
-    val accountStore = mainApplication.accountStore
+class MainActivity: BaseActivity() {
+    lateinit var binding: ActivityMainBinding
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
-        binding = ActivityMainBinding.inflate(layoutInflater);
+        binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
-        val listView = binding.list
-        listView.adapter = ArrayAdapter(this, android.R.layout.activity_list_item, android.R.id.text1, arrayOf("Rss Client", "Authorization", "Run Request", "Clear cache", "Load Sets"))
-        listView.onItemClickListener = OnItemClickListener { _, _, position, _ ->
-            if (position == 0) {
-                showRssClient()
-            } else if (position == 1) {
-                showAuthorization()
-            } else if (position == 2) {
-                scope.launch { requestUser() }
-            } else if (position == 3) {
-                clearCache()
-            } else if (position == 4) {
-                scope.launch { loadSets(true) }
-            }
-        }
-    }
-
-    private fun showRssClient() {
-        val intent = Intent(this, MainRssActivity::class.java)
-        startActivity(intent)
-    }
-
-    private fun showAuthorization() {
-        val authTask: Task = object : TaskImpl() {
-            override suspend fun startTask() {
-                val account = Networks.createAccount(Network.Quizlet)
-                val creds = withContext(Dispatchers.IO) {
-                    account.authorize()
+        supportFragmentManager.fragmentFactory = object : FragmentFactory() {
+            override fun instantiate(classLoader: ClassLoader, className: String): Fragment {
+                if (LauncherFragment.javaClass.name == className) {
+                    return LauncherFragment()
                 }
 
-                Log.d(TAG, "showAuthorization onFinished " + creds.isValid)
+                return super.instantiate(classLoader, className)
             }
         }
-        taskManager.addTask(authTask)
-    }
 
-    private suspend fun requestUser() {
-        if (service == null) {
-            initService()
-        }
-        val builder = HttpUrlConnectionBuilder()
-        builder.setUrl("https://api.foursquare.com/v2/users/self?v=20140806&m=foursquare")
-        val cmd: HttpServiceCommand<*> = HttpServiceCommand<String>(builder, BytesStringConverter())
-        val cache = SimpleCache(storage!!)
-        cache.cacheMode = Cache.CacheMode.CHECK_CACHE_IF_ERROR_THEN_LOAD
-        cmd.setCacheClient(cache)
-        service!!.runCommand(cmd, true)
-    }
-
-    private fun initService() {
-        service = SimpleService()
-
-        val accounts = accountStore.getAccounts(Network.Quizlet.ordinal)
-        val serviceAccount = if (accounts.size > 0) {
-            accounts[0]
-        } else {
-            Networks.createAccount(Network.Quizlet)
-        }
-
-        service!!.account = serviceAccount
-        // TODO: we need the solution for id
-        service!!.setServiceCommandProvider(ServiceTaskProvider())
-        service!!.setServiceCommandRunner(ServiceTaskRunner(taskManager, "31234"))
-        storage = serviceCache
-    }
-
-    private val serviceCache: Storage
-        get() {
-            val cacheDir = getDir("ServiceCache", Context.MODE_PRIVATE)
-            return DiskStorage(cacheDir)
-        }
-
-    private fun clearCache() {
-        try {
-            accountStore.removeAll()
-            serviceCache.removeAll()
-        } catch (e: Exception) {
-            e.printStackTrace()
-        }
-    }
-
-    private suspend fun loadSets(useCache: Boolean) {
-        mainApplication.quizletService.loadSets(null)
-    }
-
-    override fun onCreateOptionsMenu(menu: Menu): Boolean { // Inflate the menu; this adds items to the action bar if it is present.
-        menuInflater.inflate(R.menu.menu_main, menu)
-        return true
-    }
-
-    override fun onOptionsItemSelected(item: MenuItem): Boolean { // Handle action bar item clicks here. The action bar will
-// automatically handle clicks on the Home/Up button, so long
-// as you specify a parent activity in AndroidManifest.xml.
-        val id = item.itemId
-        return if (id == R.id.action_settings) {
-            true
-        } else super.onOptionsItemSelected(item)
-    }
-
-    companion object {
-        private const val TAG = "MainActivity"
+        val fragment = supportFragmentManager.fragmentFactory.instantiate(classLoader, LauncherFragment.javaClass.name)
+        supportFragmentManager.beginTransaction()
+                .setReorderingAllowed(true)
+                .add(binding.fragmentContainerView.id, fragment, "launcher")
+                .commitNowAllowingStateLoss()
     }
 }
