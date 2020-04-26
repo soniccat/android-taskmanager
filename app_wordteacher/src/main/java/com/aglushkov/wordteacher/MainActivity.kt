65656e1aa9316279355adf5c7ce19405c226bc90
Alexey Glushkov <soniccat@list.ru>
2020-04-26 19:40:10 +0300

    testing OpenNLP

--- app_wordteacher/src/main/java/com/aglushkov/wordteacher/MainActivity.kt
+++ app_wordteacher/src/main/java/com/aglushkov/wordteacher/MainActivity.kt
@@ -1,13 +1,25 @@
 package com.aglushkov.wordteacher
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import androidx.appcompat.app.AppCompatActivity
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.FragmentFactory
 import com.aglushkov.wordteacher.databinding.ActivityMainBinding
 import com.aglushkov.wordteacher.features.definitions.view.DefinitionsFragment
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.SupervisorJob
+import kotlinx.coroutines.launch
+import opennlp.tools.postag.POSModel
+import opennlp.tools.postag.POSTaggerME
+import opennlp.tools.tokenize.Tokenizer
+import opennlp.tools.tokenize.TokenizerME
+import opennlp.tools.tokenize.TokenizerModel
+
 
 class MainActivity : AppCompatActivity() {
+    private val testScope = CoroutineScope(SupervisorJob() + Dispatchers.IO)
     lateinit var binding: ActivityMainBinding
 
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -34,89 +46,28 @@ class MainActivity : AppCompatActivity() {
                     .commitAllowingStateLoss()
         }
 
-//        mainScope.launch {
-//            configRepository.flow.collect {
-//                Log.d("Config", "" + it)
-//            }
-//        }
-//
-//        mainScope.launch {
-//            serviceRepository.flow.collect {
-//                Log.d("Services", "" + it)
-//            }
-//        }
-//
-//        mainScope.launch {
-//            serviceRepository.flow.flowOn(Dispatchers.IO).collect {
-//                Log.d("Services", "" + it)
-//            }
-//        }
-//
-//        mainScope.launch {
-//            //serviceRepository.flow.first { it.isLoaded() }
-//            val defs = wordRepository.define("owl").flow.first { it.isLoaded() }
-//            Log.d("Definitions", "" + defs)
-//        }
+        System.setProperty("org.xml.sax.driver","org.xmlpull.v1.sax2.Driver");
+        testScope.launch {
+            resources.openRawResource(R.raw.en_token).use { modelIn ->
+                val tokenizerModel = TokenizerModel(modelIn)
+                val tokenizer: Tokenizer = TokenizerME(tokenizerModel)
+                val tokens = tokenizer.tokenize("Of course this is all on the command line. Many people use the models directly in their Java code by creating SentenceDetector and Tokenizer objects and calling their methods as appropriate. The following section will explain how the Tokenizers can be used directly from java. He's great. I like Mike's apartment.");
+                Log.d("ttt", "tokenized: " + tokens)
 
-//        val owlBotConfig = ServiceConfig(listOf(getString(R.string.owlbot_base_url)),
-//                listOf(getString(R.string.owlbot_token)))
-//        val service = OwlBotService.createWordTeacherWordService(owlBotConfig.baseUrls.first(), owlBotConfig.keys.first())
+                resources.openRawResource(R.raw.en_pos_maxent).buffered().use {
+                    try {
+                        val posTaggerModel = POSModel(it)
+                        val tagger = POSTaggerME(posTaggerModel)
+                        val tags = tagger.tag(tokens);
 
-//        val googleConfig = ServiceConfig(listOf(getString(R.string.goolge_base_url)),
-//                emptyList(),
-//                ServiceMethodParams(mapOf(GoogleService.EntriesMethod to mapOf(GoogleService.EntriesMethodLang to "en"))))
-//        val service = GoogleService.createWordTeacherWordService(googleConfig.baseUrls.first(), googleConfig.methodOptions)
-
-//        val wordLinkConfig = Config(
-//                Config.Type.Wordlink,
-//                listOf(getString(R.string.wordlink_base_url)),
-//                listOf(getString(R.string.wordlink_key)),
-//                ServiceMethodParams(mapOf(WordLinkService.Definitions to mapOf(
-//                        WordLinkService.DefinitionsSourceDictionaries to WordLinkService.Dictionary.Ahd5.value,
-//                        WordLinkService.DefinitionsIncludeRelated to true.toString(),
-//                        WordLinkService.DefinitionsIncludeTags to true.toString(),
-//                        WordLinkService.DefinitionsLimit to 11.toString(),
-//                        WordLinkService.DefinitionsUseCanonical to true.toString()
-//                ))))
-//        val service = WordLinkService.createWordTeacherWordService(
-//                wordLinkConfig.baseUrls.first(),
-//                wordLinkConfig.keys.first(),
-//                wordLinkConfig.methodOptions)
-
-//        val yandexConfig = ServiceConfig(listOf(getString(R.string.yandex_base_url)),
-//                listOf(getString(R.string.yandex_key)),
-//                ServiceMethodParams(mapOf(YandexService.Lookup to
-//                        mapOf(YandexService.LookupLang to "en-en",
-//                              YandexService.LookupUi to "en",
-//                              YandexService.LookupFlags to "4"
-//                            ))))
-//        val service = YandexService.createWordTeacherWordService(yandexConfig.baseUrls.first(),
-//                yandexConfig.keys.first(),
-//                yandexConfig.methodOptions)
-
-//        testScope.launch {
-//            try {
-//                val response = service.define("owl")
-//            Log.d("owlbot", "response : $response")
-
-//            val response = wordLinkService.definitions("owl",
-//                    "wiktionary,century",
-//                    20,
-//                    null,
-//                    true,
-//                    true,
-//                    true)
-//            Log.d("wordlink", "response : $response")
-
-//            val response = yandexService.definitions("owl", "en-en", "en", 4)
-//            Log.d("yandex", "response : $response")
-
-//            val response = googleService.definitions("hello", "en")
-//                Log.d("google", "response : $response")
-//            } catch (ex: Exception) {
-//                Log.d("abc", ex.message)
-//                ex.printStackTrace()
-//            }
-//        }
+                        val tokens2 = tokenizer.tokenize("Of course this is all on the command line. Many people use the models directly in their Java code by creating SentenceDetector and Tokenizer objects and calling their methods as appropriate. The following section will explain how the Tokenizers can be used directly from java. He's great. I like Mike's apartment.");
+                        val tags2 = tagger.tag(tokens2);
+                        Log.d("ttt", "tags: " + tags)
+                    } catch (ex: Exception) {
+                        ex.printStackTrace()
+                    }
+                }
+            }
+        }
     }
 }
